f636d777a2ed31ca6abaad9d1484cc06b8753030
Ricardo <ricardo-pinto@web.de>
2024-10-31 13:46:26 +0100

    first commit

---
+++ models/movable-object.class.js
@@ -0,0 +1,20 @@
+class MovableObject {
+  x = 120;
+  y= 250;
+  img;
+  width = 150;
+  height = 100;
+
+  loadImage(path) {
+    this.img = new Image();
+    this.img.src = path;
+  }
+
+  moveRight() {
+
+  }
+
+  moveLeft() {
+    
+  }
+}
