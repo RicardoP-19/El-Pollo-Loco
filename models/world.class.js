0bc607e083c59747e273bc8f28c1601c45895372
Ricardo <ricardo-pinto@web.de>
2024-10-31 15:55:49 +0100

    object-position-and-height

--- models/world.class.js
+++ models/world.class.js
@@ -1,16 +1,23 @@
 class World {
   character = new Character();
+
   enemies = [
     new Chicken(),
     new Chicken(),
     new Chicken(),
   ];
+
   clouds = [
     new Cloud()
   ];
+
   backgroundObjects = [
-    new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0)
+    new BackgroundObject('img/5_background/layers/air.png', 0),
+    new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
+    new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
+    new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0)
   ];
+
   canvas;
   ctx;
 
@@ -22,12 +29,10 @@ class World {
 
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    
+    this.addObjectsToMap(this.backgroundObjects);
     this.addToMap(this.character);
     this.addObjectsToMap(this.clouds);
     this.addObjectsToMap(this.enemies);
-    this.addObjectsToMap(this.backgroundObjects);
-
     let self = this
     requestAnimationFrame(function() {
       self.draw();
