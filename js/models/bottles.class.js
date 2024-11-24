9a03af28854257cc41d057959575ccd2c5884c94
Ricardo <ricardo-pinto@web.de>
2024-11-24 16:22:01 +0100

    add_bottles

---
+++ js/models/bottles.class.js
@@ -0,0 +1,18 @@
+class Bottle extends DrawableObject {
+  world;
+  y = 370;
+  width = 65;
+  height = 65;
+  offset = {
+    top: 0,
+    left: 20,
+    right: 20,
+    bottom: 0
+};
+
+  constructor() {
+    super();
+    this.loadImage('assets/img/6_salsa_bottle/2_salsa_bottle_on_ground.png');
+    this.x = 200 + Math.random() * 2000;
+  }
+}
